e93d242f5625928b241a037c9db1641ec3daaa9f
Yaw Boakye <wheresyaw@gmail.com>
2016-09-30 03:38:51 +0000

    Move timeline item attributes to parent model

--- app/resources/v1/text_timeline_item_resource.rb
+++ app/resources/v1/text_timeline_item_resource.rb
@@ -1,5 +1,7 @@
 class V1::TextTimelineItemResource < V1::TimelineItemResource
-  attributes :item_text,
-             :created_at,
-             :updated_at
+  class << self
+    def sortable_fields(context)
+      [:created_at]
+    end
+  end
 end
--- app/resources/v1/timeline_item_resource.rb
+++ app/resources/v1/timeline_item_resource.rb
@@ -1,3 +1,7 @@
 class V1::TimelineItemResource < V1::ReactionableResource
   immutable
+
+  attributes :item_text,
+             :created_at,
+             :update_at
 end
